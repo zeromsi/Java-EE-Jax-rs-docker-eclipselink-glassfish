a00140b07a37da14e9225883da4a78fc094dabfe
Admin <Admin@Intern>
2018-04-02 17:55:12 +0600

    one

---
+++ src/main/java/App/MessageBean.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package App;
+
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Admin
+ */
+
+public class MessageBean {
+    
+}
